3eabca72577224ee273a14cd8b889fc90684e93b
Enzo Lanne <enzolanne@gmail.com>
2024-12-04 16:47:53 +0100

    message

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   # Routes pour les catégories
   resources :categories
 
+  resources :users
+
+
   # Routes pour les DaddyServices
   resources :daddy_services do
     member do
